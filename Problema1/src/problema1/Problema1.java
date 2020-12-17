e651483d52a053b11001726b6e227e7a5d0b34d1
robinson perez <linkheavy@hotmail.com>
2020-12-16 22:53:02 -0600

    nuevo proyecto

---
+++ Problema1/src/problema1/Problema1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1;
+
+/**
+ *
+ * @author linkh
+ */
+public class Problema1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        problema1GUI gui = new problema1GUI();
+        gui.setVisible(true);
+    }
+    
+}
