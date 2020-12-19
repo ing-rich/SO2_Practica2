df6815775b870c991817b94f7ef2e3c9325fb39f
robinson perez <linkheavy@hotmail.com>
2020-12-19 16:26:10 -0600

    integrar uso del portero

--- Problema1/src/problema1/Filosofo.java
+++ Problema1/src/problema1/Filosofo.java
@@ -18,111 +18,90 @@ import javax.swing.JLabel;
 public class Filosofo extends Thread {
 
     private JLabel etiquetaFilosofo;
-    private int estado;
     private Tenedor tenedorIzq;
     private Tenedor tenedorDer;
+    private Portero portero;
+    private Boolean iniciar;
     public Filosofo(JLabel etiquetaFilosofo) {
         this.etiquetaFilosofo = etiquetaFilosofo;
-        this.estado=1;
     }
-    
-    public Filosofo(JLabel filosofoLbl, Tenedor tenedorIzq,Tenedor tenedorDer){
+
+    public Filosofo(JLabel filosofoLbl, Tenedor tenedorIzq, Tenedor tenedorDer) {
         this.etiquetaFilosofo = filosofoLbl;
         this.tenedorIzq = tenedorIzq;
         this.tenedorDer = tenedorDer;
-        this.estado = 1;
+    }
+
+    public Filosofo(JLabel filosofoLbl, Tenedor tenedorIzq, Tenedor tenedorDer,Portero portero) {
+        this.etiquetaFilosofo = filosofoLbl;
+        this.tenedorIzq = tenedorIzq;
+        this.tenedorDer = tenedorDer;
+        this.portero=portero;
+        iniciar = true;
     }
     
-    private void cambiarColor(int codigo){
+    private void cambiarColor(int codigo) {
         Color color = null;
-        switch(codigo){
+        switch (codigo) {
             case 1: //pensando
                 color = Color.BLUE;
-                this.estado = 1;
                 break;
             case 2: //comiendo
                 color = Color.GREEN;
-                this.estado = 2;
                 break;
-            case 3: //con un tenedor
-                color = Color.YELLOW;
-                this.estado = 3;
-                break;
-            case 4: //entro al comedor
+            case 3: //entro al comedor
                 color = Color.WHITE;
-                this.estado = 4;
                 break;
         }
         this.etiquetaFilosofo.setBackground(color);
     }
 
-    private int tomarTenedores(){
-        Boolean tomarDerecha = this.tenedorDer.tomarTenedor();
-        Boolean tomarIzq;
-        if(tomarDerecha){
-            tomarIzq = tenedorIzq.tomarTenedor();
-            if(tomarIzq){
-                return 1; //puede comer
-            } else {
-                return 2; // solo tiene un tenedor
-            }
-        } else {
-            return 0; //sin tenedores
-        }
+    private void tomarTenedores() {
+        this.tenedorIzq.tomarTenedor();
+        this.tenedorDer.tomarTenedor();
     }
-    
-    private void pensar(){
+
+    private void pensar() {
         this.cambiarColor(1);
         Random r = new Random();
         try {
-            Filosofo.sleep(r.nextInt(1000) + 500);
+            Filosofo.sleep(r.nextInt(2000) + 500);
         } catch (InterruptedException ex) {
             Logger.getLogger(Filosofo.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private void comer(){
+
+    private void comer() {
+        tomarTenedores();
         this.cambiarColor(2);
         Random r = new Random();
         try {
-            Filosofo.sleep(r.nextInt(1000) + 500);
+            Filosofo.sleep(r.nextInt(2000) + 500);
         } catch (InterruptedException ex) {
             Logger.getLogger(Filosofo.class.getName()).log(Level.SEVERE, null, ex);
         }
         this.tenedorDer.dejarTenedor();
         this.tenedorIzq.dejarTenedor();
-    }
-    
-    private void esperarTenedor(){
-        this.cambiarColor(3);
-        Random r = new Random();
-        try {
-            Filosofo.sleep(r.nextInt(1000) + 500);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Filosofo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        this.tenedorDer.dejarTenedor();
-    }
-    
-    private void acciones(int estado){
-        switch(estado){
-            case 0://no tiene tenedores, ira a pensar
-                pensar();
-                break;
-            case 1: //puede comer
-                comer();
-                break;
-            case 2:
-                esperarTenedor();
-                break;
-        }
+        this.cambiarColor(1);
     }
     
     @Override
     public void run() {
-        while (true) {
-            acciones(tomarTenedores());
-            System.out.println("corriendo");
+        while (iniciar) {
+            try {
+                pensar();
+                this.portero.ingresarFilosofo();
+                this.cambiarColor(3);
+                comer();
+                this.portero.sacarFilosofo();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Filosofo.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+        System.out.println("Proceso finalizado!");
+    }
+    
+    public void detener(){
+        this.iniciar = false;
     }
 }
