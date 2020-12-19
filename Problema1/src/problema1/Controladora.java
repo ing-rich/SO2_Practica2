7bb2b4f3ea220ce9677e0eeb7e6dd44bb83b3aeb
robinson perez <linkheavy@hotmail.com>
2020-12-19 16:25:08 -0600

    agregar funcion para detener la simulacion

--- Problema1/src/problema1/Controladora.java
+++ Problema1/src/problema1/Controladora.java
@@ -16,11 +16,13 @@ public class Controladora {
     private JLabel [] filosofosLbl;
     private Filosofo [] filosofos;
     private Tenedor [] tenedores;
+    private Portero portero;
     public Controladora(JLabel [] tenedoresLbl, JLabel [] filosofosLbl){
         filosofos = new Filosofo[5];
         tenedores = new Tenedor[5];
         this.tenedoresLbl = tenedoresLbl;
         this.filosofosLbl = filosofosLbl;
+        this.portero = new Portero();
     }
     
     public void iniciarSimulacion(){
@@ -28,10 +30,16 @@ public class Controladora {
             tenedores[i] = new Tenedor(tenedoresLbl[i],i);
         }
         for(int i=0; i<5; i++){
-            filosofos[i] = new Filosofo(filosofosLbl[i],tenedores[(i+4)%5],tenedores[i]);
+            filosofos[i] = new Filosofo(filosofosLbl[i],tenedores[(i+4)%5],tenedores[i],this.portero);
         }
         for(int i=0; i<5;i++){
             filosofos[i].start();
+        }
+    }
+    
+    public void detenerSimulacion(){
+        for(int i=0;i <5; i++){
+            filosofos[i].detener();
         }
     }
 }
